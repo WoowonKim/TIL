ba458d344173ba148b135ea5b44c1a8b1e7d2cf8
WoowonKim <dyffh1031@gmail.com>
2022-01-25 21:41:48 +0900

    0124,25 work

--- py_prob/BJ1003.py
+++ py_prob/BJ1003.py
@@ -1,21 +1,22 @@
 global cnt1, cnt0
-cnt0 = 0
 cnt1 = 0
 
 def fibo(N):
     global cnt0
     global cnt1
     if N == 1:
-        cnt1 += 1
         return 1
     elif N == 0:
         cnt0 += 1
         return 0
     else:
         return fibo(N-1) + fibo(N-2)
+    
 
-# res = fibo(22)
-# print(cnt0,cnt1)
 T = int(input())
 for i in range(T):
-    
+    N = int(input())
+    cnt0 = 0
+    cnt1 = 0
+    a = fibo(N)
+    print(cnt0,a)
