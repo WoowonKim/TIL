4b787974fb077c095d8221d5b364548395bded73
WoowonKim <dyffh1031@gmail.com>
2022-07-09 22:02:48 +0900

    styled-components

--- React_Prac/react_shop/src/routes/Detail.js
+++ React_Prac/react_shop/src/routes/Detail.js
@@ -1,9 +1,24 @@
 import {useParams} from "react-router-dom"
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background : ${props => props.bg};
+  color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
+  padding : 10px;
+`
+let BlackBox = styled.div`
+  background : grey;
+  padding : 20px
+`
+
 function Detail(props){
   let {id} = useParams();
   let item = props.shoe.find(x => x.id == id)
   return(
     <div className="container">
+      <BlackBox>
+        <YellowBtn bg='blue'>버튼</YellowBtn>
+      </BlackBox>
       <div className="row">
         <div className="col-md-6">
           <img src={item.imgSrc} width="100%" />
